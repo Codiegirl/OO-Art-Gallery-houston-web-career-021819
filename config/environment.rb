46a90825ea90dfb162ef1375d1585e62d0e7a274
github username <github email address>
2019-02-27 23:09:27 -0600

    done

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,12 @@ require_relative '../app/models/artist.rb'
 require_relative '../app/models/gallery.rb'
 require_relative '../app/models/painting.rb'
 
-artist1 = Artist.new("Rochell", 15)
-artist2 = Artist.new("Josh", 89)
-artist2 = Artist.new("Jim", 10000)
-gallery1 = Gallery.new("Apex", "Houston")
-new_painting1 = Painting.new("Stars", 1000000, artist1, gallery1)
-new_painting2 = Painting.new("Moon", 2000000, artist1, gallery1)
+artist1 = Artist.new(name: "Rochell", years_experience: 15)
+artist2 = Artist.new(name: "Josh", years_experience: 89)
+artist3 = Artist.new(name: "Jim", years_experience: 10000)
+gallery1 = Gallery.new(name: "Apex", city: "Houston")
+new_painting1 = Painting.new(title: "Stars", price: 73, artist: artist1, gallery: gallery1)
+new_painting2 = Painting.new(title: "Moon", price: 68, artist: artist2, gallery: gallery1)
 
 
 
